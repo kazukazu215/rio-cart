994d2262339276627d46784548299f4c47e02c84
kazukau215 <21511252kh@tama.ac.jp>
2016-05-20 19:54:09 +0900

    test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start() {
+		// Workaround: Give HMD time to initialize (2 frame skip)
+		yield return null;
+		yield return null;
+		// change VRSettings as needed
+		UnityEngine.VR.VRSettings.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
